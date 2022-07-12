072e008ba20691cacfe0bf390dce21cf2bbd34c8
Joe Froula <jmfroula@gmail.com>
2022-07-12 14:51:53 -0700

    testing buttons

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 function Buttons({store}) {
   return (
     <div className="flex justify-center">
-       <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded" onClick={store.submitGuess}>Enter</button>
-       <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded" onClick={store.handleBackSpace}>Delete</button>
+
+       <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded m-3" onClick={store.handleBackSpace}>Delete</button>
+       <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded m-3" onClick={store.submitGuess}>Enter</button>
+       
 
     </div>
   )
