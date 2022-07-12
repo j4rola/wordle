364ca17a7bc729a4e785c67657d72cf479fdcf7a
Joe Froula <jmfroula@gmail.com>
2022-07-12 13:35:27 -0700

    updates ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,11 +21,11 @@ import PuzzleStore from '../stores/PuzzleStore'
 
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center bg-gray-600">
-      <h1 className="text-6xl font-bold uppercase">Eldrow</h1><h2 className="mb-4">A practice arena for your favorite word puzzle</h2>
+      <h1 className="text-6xl font-bold text-gray-200 uppercase">Eldrow</h1><h2 className="mb-4">A practice arena for your favorite word puzzle</h2>
       {store.guesses.map((x, i) => (<Guess key={i} word={store.word} guess={store.guesses[i]} isGuessed={i < store.currentGuess} />))}
       {store.won && <h1>Nice Job</h1>}
-      {store.lost && <h1>Better luck next time</h1>}
-      {(store.won || store.lost) && (<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={store.init}>Play Again</button>)}
+      {store.lost && <h1 className="my-3">Better luck next time</h1>}
+      {(store.won || store.lost) && (<button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" onClick={store.init}>Play Again</button>)}
       
       <Qwerty store={store}/>  
     
